8330ad03fa0e94228b14967bd1ad09b0baf79e0a
Matt Miller <matt@binary.com>
2015-01-05 15:45:57 -0500

    Respect the force_reload flag

--- lib/YAML/CacheLoader.pm
+++ lib/YAML/CacheLoader.pm
@@ -18,15 +18,17 @@ use YAML qw( LoadFile );
 sub LoadFile {
     my ($path, $force_reload) = @_;
 
-    my $file_loc = path($path)->canonpath;             # realpath would be more accurate, but slower.
+    my $file_loc = path($path)->canonpath;    # realpath would be more accurate, but slower.
 
-    my $from_cache = Cache::RedisDB->get(CACHE_NAMESPACE, $file_loc);
-    return decode_sereal($from_cache) if ($from_cache and looks_like_sereal($from_cache));    # Happy path
+    if (not $force_reload) {
+        my $from_cache = Cache::RedisDB->get(CACHE_NAMESPACE, $file_loc);
+        return decode_sereal($from_cache) if ($from_cache and looks_like_sereal($from_cache));    # Happy path
+    }
 
     # Looks like we'll need to actually do some work, then.
-    my $structure = LoadFile($file_loc);                                                      # Let this fail in whatever ways it might.
+    my $structure = LoadFile($file_loc);                                                          # Let this fail in whatever ways it might.
 
-    Cache::RedisDB->set(CACHE_NAMESPACE, encode_sereal($structure), CACHE_SECONDS) if ($structure);
+    Cache::RedisDB->set(CACHE_NAMESPACE, $file_loc, encode_sereal($structure), CACHE_SECONDS) if ($structure);
 
     return $structure;
 }
